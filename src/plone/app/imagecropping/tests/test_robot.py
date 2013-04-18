7669639e4162d5d52592bff1b4044c1555e0fdbf
Daniel Widerin <daniel@widerin.net>
2013-04-18 20:24:36 +0200

    Make tests work in Plone 4.1
    -  Rename acceptance to robot to align plone.app.robotframework guidelines

---
+++ src/plone/app/imagecropping/tests/test_robot.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from plone.app.imagecropping.testing import \
+    PLONE_APP_IMAGECROPPING_ROBOT
+from plone.testing import layered
+
+import os
+import robotsuite
+import unittest
+
+
+def test_suite():
+    suite = unittest.TestSuite()
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    robot_dir = os.path.join(current_dir, 'robot')
+    robot_tests = [os.path.join('robot', doc) for doc in
+                   os.listdir(robot_dir) if doc.endswith('.txt') and
+                   doc.startswith('test_')]
+    for test in robot_tests:
+        suite.addTests([
+            layered(robotsuite.RobotTestSuite(test),
+                    layer=PLONE_APP_IMAGECROPPING_ROBOT),
+        ])
+    return suite
