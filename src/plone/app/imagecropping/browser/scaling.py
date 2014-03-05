362674ff33c9fb15bf258cd7c4fa37926ef5c237
Tom Gross <itconsense@gmail.com>
2014-03-05 22:15:15 +0100

    implement scaling for plone.namedfile

--- src/plone/app/imagecropping/browser/scaling.py
+++ src/plone/app/imagecropping/browser/scaling.py
@@ -6,12 +6,60 @@ from plone.app.imaging.scaling import ImageScaling as BaseImageScaling
 
 class ImageScaling(BaseImageScaling):
 
+    _rescale = True
+
     def modified(self):
-        cropped = IAnnotations(self.context).get(PAI_STORAGE_KEY)
-        if not cropped:
+        if self._rescale:
             return super(ImageScaling, self).modified()
         else:
             return 1
+    
+    def scale(self,
+              fieldname=None,
+              scale=None,
+              height=None,
+              width=None,
+              **parameters):
+        cropped = IAnnotations(self.context).get(PAI_STORAGE_KEY)
+        if '%s_%s' % (fieldname, scale) in cropped:
+            self._rescale = False
+        else:
+            self._rescale = True
+        return super(ImageScaling, self).scale(fieldname, scale, height, width, **parameters)
 
 
-# XXX need this for plone.namedfile and NEWSItem too
+try:
+    from plone.namedfile.scaling import ImageScaling as NFImageScaling
+
+    from plone.namedfile.interfaces import IImageScaleTraversable
+    from plone.app.imagecropping.interfaces import IImageCropping
+
+    class IImageCroppingScale(IImageScaleTraversable, IImageCropping):
+        pass
+
+    class NamedfileImageScaling(NFImageScaling):
+
+	_rescale = True
+
+	def modified(self):
+	    if self._rescale:
+		return super(ImageScaling, self).modified()
+	    else:
+		return 1
+
+	def scale(self,
+		  fieldname=None,
+		  scale=None,
+		  height=None,
+		  width=None,
+                  direction='thumbnail',
+		  **parameters):
+	    cropped = IAnnotations(self.context).get(PAI_STORAGE_KEY)
+	    if '%s_%s' % (fieldname, scale) in cropped:
+		self._rescale = False
+	    else:
+		self._rescale = True
+	    return super(ImageScaling, self).scale(fieldname, scale, height, width, direction, **parameters)
+
+except ImportError:
+    pass
