1ec1f474cb19dca85b13e4887b407b38469ac4d9
Jens W. Klein <jk@kleinundpartner.at>
2014-09-03 10:24:37 +0200

    use new marker here

--- src/plone/app/imagecropping/subscribers.py
+++ src/plone/app/imagecropping/subscribers.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 from plone.app.imagecropping import PAI_STORAGE_KEY
-from plone.app.imagecropping.interfaces import IImageCropping
+from plone.app.imagecropping.interfaces import IImageCroppingMarker
 from zope.annotation.interfaces import IAnnotations
 from zope.component import adapter
 from zope.lifecycleevent.interfaces import IObjectCopiedEvent
 
 
-@adapter(IImageCropping, IObjectCopiedEvent)
+@adapter(IImageCroppingMarker, IObjectCopiedEvent)
 def apply_crops_after_copy(context, event):
     crops = IAnnotations(event.original).get(PAI_STORAGE_KEY)
 
