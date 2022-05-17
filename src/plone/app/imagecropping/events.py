ddacd8a571531bca037ec41d317b7dbf66f7ae4d
Manuel Reinhardt <reinhardt@syslab.com>
2022-05-17 16:35:04 +0200

    Fixed imports for Plone 6

--- src/plone/app/imagecropping/events.py
+++ src/plone/app/imagecropping/events.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 from plone.app.imagecropping.interfaces import ICroppingInfoChangedEvent
 from plone.app.imagecropping.interfaces import ICroppingInfoRemovedEvent
-from zope.component.interfaces import ObjectEvent
+from zope.interface.interfaces import ObjectEvent
 from zope.interface import implementer
 
 
